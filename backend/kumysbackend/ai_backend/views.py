221b59a26bf87bd1d9676bf6d854625696b36e1b
YergaliyevIssa <isa.ergaliev9@gmail.com>
2024-10-18 20:24:01 +0300

    added swagger

--- backend/kumysbackend/ai_backend/views.py
+++ backend/kumysbackend/ai_backend/views.py
@@ -2,8 +2,32 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
 class Recommendations(APIView):
 
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            required=["text"],
+            properties={
+                "text": openapi.Schema(type=openapi.TYPE_STRING)
+            }
+        ),
+        responses={
+            200: openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "result": openapi.Schema(type=openapi.TYPE_STRING),
+                    "recommendations":  openapi.Schema(
+                        type=openapi.TYPE_ARRAY,
+                        items=openapi.Schema(type=openapi.TYPE_STRING),
+                    ),
+                }
+            )
+        }
+    )
     def post(self, request):
         text = request.data["text"]
 
