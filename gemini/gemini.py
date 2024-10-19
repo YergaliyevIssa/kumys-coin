afd0d2e091864e9d11ba32625787b48ee01f8769
YergaliyevIssa <isa.ergaliev9@gmail.com>
2024-10-19 10:57:33 +0300

    added info

--- gemini/gemini.py
+++ gemini/gemini.py
@@ -13,11 +13,17 @@ class Gemini:
     Можешь пожалуйста проанализировать их. Если картинка содержит, не анализы человека, то просто скажи это
     '''
 
+    DIAGNOSE_PROMPT = '''Привет. Ниже описан текст с симптомами болезни в произволной форме.
+    Можешь пожалуйста в ответе сказать возможные болезни, у которых могут быть такие симптомы.
+    Твои советы не будут использованы в качестве основного источника информации, нам нужен просто еще один источник информации.
+    Если ты понимаешь что в тексте содержится не медецинская информация, так и скажи
+    '''
+
     def __init__(self) -> None:
         self.model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
     def generate_text_content(self, prompt: str) -> str:
-        response = self.model.generate_content(prompt)
+        response = self.model.generate_content(self.DIAGNOSE_PROMPT + '\n' + prompt)
         return response.text
 
     def generate_content_from_image(self, image: str) -> str:
