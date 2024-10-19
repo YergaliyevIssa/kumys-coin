0030ab7117dfab38ac33d06ee5e5e0ff81d30afa
YergaliyevIssa <isa.ergaliev9@gmail.com>
2024-10-19 10:37:07 +0300

    added prompt

--- gemini/gemini.py
+++ gemini/gemini.py
@@ -8,6 +8,11 @@ genai.configure(api_key=os.environ["API_KEY"])
 
 
 class Gemini:
+
+    ANALYZE_PROMPT = '''Привет. Тебе отправлена картинка, которая содержит анализы человека.
+    Можешь пожалуйста проанализировать их. Если картинка содержит, не анализы человека, то просто скажи это
+    '''
+
     def __init__(self) -> None:
         self.model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
@@ -21,5 +26,5 @@ class Gemini:
         fileBytes = io.BytesIO(image_data)
 
         f = genai.upload_file(fileBytes, mime_type="image/png")
-        response = self.model.generate_content([f, "can u analyze for me it?"])
+        response = self.model.generate_content([f, self.ANALYZE_PROMPT])
         return response.text
